a039c1800dedcd1d66cfd6501e2969f423f984b8
Grant Hutchins <grant.hutchins@srp-ok.com>
2020-01-22 14:44:31 -0600

    Refer to appropriate module for params
    Co-authored-by: Grant Hutchins <grant.hutchins@srp-ok.com>
    Co-authored-by: Adam Milligan <adam.milligan@srp-ok.com>

--- lib/rails_improvements/railtie.rb
+++ lib/rails_improvements/railtie.rb
@@ -4,7 +4,7 @@ module RailsImprovements
   class Railtie < Rails::Railtie
     initializer 'rails_improvements.action_controller.parameters.hash_polymorphism' do
       require 'rails_improvements/action_controller/parameters/hash_polymorphism'
-      ActionController::Parameters.instance_eval do
+      ::ActionController::Parameters.instance_eval do
         include RailsImprovements::ActionController::Parameters::HashPolymorphism
       end
     end
